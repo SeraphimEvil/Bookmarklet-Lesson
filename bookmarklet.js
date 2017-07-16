b7893120ee4b0300513717953d22cfee763f853c
Sergey Ostapovec <seraphimaion@gmail.com>
2017-07-17 00:01:49 +0300

    добавил проверку того кто оставил комментарий

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,9 +4,19 @@ javascript:
 (function() {
 	var closedComment = document.querySelectorAll('.js-comment-container.outdated-comment');
 	const studentName = 'SeraphimEvil';
-	const mentorName = 'aalexeev239';
+	const mentorName = 'lizzzzzy';
 
 	for (var i = closedComment.length - 1; i>=0; i--) {
+		var commentAuthor = document.querySelector('.review-comment a.author').innerHTML;
+
 		closedComment[i].classList.remove('outdated-comment');
+
+		if (commentAuthor == studentName) {
+			console.log('Комментарий студента');
+		} else if (commentAuthor == mentorName) {
+			console.log('Комментарий наставника')
+		} else {
+			console.log('Нет комментария')
+		}
 	};
 })();
