6c68e7dd7a885351e0ce24a7484d5f497e4c0ee8
Sergey Ostapovec <seraphimaion@gmail.com>
2017-07-16 23:29:42 +0300

    add constatnt users

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,14 +1,12 @@
 'use strict';
 
 javascript:
-(function openComments() {
+(function() {
 	var closedComment = document.querySelectorAll('.js-comment-container.outdated-comment');
+	const studentName = 'SeraphimEvil';
+	const mentorName = 'aalexeev239';
 
 	for (var i = closedComment.length - 1; i>=0; i--) {
 		closedComment[i].classList.remove('outdated-comment');
 	};
 });
-
-
-// javascript:(function() {var closedComment = document.querySelectorAll('.js-comment-container.outdated-comment');for (var i = closedComment.length - 1; i>=0; i--) {closedComment[i].classList.remove('outdated-comment');};})();
-
