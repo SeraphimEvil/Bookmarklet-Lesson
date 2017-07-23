4a55c76563bfd4f8a7b0f4c73deabb4a587c3d0b
Sergey Ostapovec <seraphimaion@gmail.com>
2017-07-24 00:08:16 +0300

    сделал правки после звонка

--- bookmarklet.js
+++ bookmarklet.js
@@ -6,21 +6,11 @@ javascript:
   const MENTOR_NAME = 'aalexeev239';
   // const MENTOR_NAME = 'lizzzzzy';
 
-  const COMMENT_CONTAINERS = document.querySelectorAll('.js-comment-container'); // ищем все комментарии, получаем NodeList...  
+  const commentContainers = document.querySelectorAll('.js-comment-container'); // ищем все комментарии, получаем NodeList...  
 
-  const COMMENT_CONTAINERS_LIST = Array.prototype.slice.call(COMMENT_CONTAINERS); // приводим NodeList к Array
+  const commentContainersList = Array.prototype.slice.call(commentContainers); // приводим NodeList к Array
 
-  const CLOSED_COMMENT_CONTAINERS_LIST = []; // массив, в котором будем хранить список закрытых комментариев
-
-  // цикл в котором помещаем все элементы с классом закрытого комментария в отдельный массив
-  for (let i = 0; i < COMMENT_CONTAINERS_LIST.length; i++) {
-    if (COMMENT_CONTAINERS_LIST[i].classList.contains('outdated-comment')) {
-      CLOSED_COMMENT_CONTAINERS_LIST.push(COMMENT_CONTAINERS_LIST[i]);
-    }
-  };
-
-  // открываем все закрытые комментарии
-  for (let i = 0; i < CLOSED_COMMENT_CONTAINERS_LIST.length; i++) {
-    CLOSED_COMMENT_CONTAINERS_LIST[i].classList.remove('outdated-comment'); 
-  };
+  const closedCommentContainersList = commentContainersList
+    .filter(element => element.classList.contains('outdated-comment'))
+    .forEach(element => element.classList.remove('outdated-comment'));
 })();
