d30bf080727de6bc2dc03b56a6d749ff498aaea2
Sergey Ostapovec <seraphimaion@gmail.com>
2017-07-27 00:09:31 +0300

    сделал стрелочную функцию фильтра

--- bookmarklet.js
+++ bookmarklet.js
@@ -6,19 +6,16 @@ javascript:
   const MENTOR_NAME = 'aalexeev239';
   const HOORAY_REACTION = 'HOORAY';
 
-  const commentContainers = document.querySelectorAll('.js-comment-container'); // ищем все комментарии, получаем NodeList...  
+  const commentContainers = document.querySelectorAll('.js-comment-container');
 
-  const commentContainersList = Array.prototype.slice.call(commentContainers); // приводим NodeList к Array
+  const commentContainersList = Array.prototype.slice.call(commentContainers);
 
   const closedCommentContainersList = commentContainersList
     .filter(element => element.classList.contains('outdated-comment'))
     .forEach(element => element.classList.remove('outdated-comment'));
 
   const reactedCommentsList = commentContainersList 
-    .filter(function(element) {
-      let reactComment = element.lastElementChild.querySelector('.review-comment:last-child .comment-reactions-options button');
-      return reactComment;
-    })
+    .filter(element => element.lastElementChild.querySelector('.review-comment:last-child .comment-reactions-options button'))
     .forEach(function(element) {
       let btnReaction = element.lastElementChild.querySelector('.review-comment:last-child .comment-reactions-options button:last-child');
       let checkMentor = btnReaction.getAttribute('aria-label');
