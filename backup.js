175762bc26a25dec001871ac1e7bbb100a119820
Sergey Ostapovec <seraphimaion@gmail.com>
2017-07-29 00:47:45 +0300

    исправление после звонка 28.07.17

--- backup.js
+++ backup.js
@@ -9,21 +9,25 @@ javascript:
   const commentContainers = document.querySelectorAll('.js-comment-container');
   const commentContainersList = Array.prototype.slice.call(commentContainers);
 
-  const closedCommentContainersList = commentContainersList
+  commentContainersList
     .filter(element => element.classList.contains('outdated-comment'))
     .forEach(element => element.classList.remove('outdated-comment'));
 
-  const reactedCommentsList = commentContainersList 
-    .filter(element => element.querySelector('.js-reactions-container .comment-reactions-options button'))
-    .forEach(function(element) {
-      let btnReaction = element.querySelector('.js-reactions-container .comment-reactions-options button:last-child');
-      let checkMentor = btnReaction.getAttribute('aria-label');
-      let checkReaction = btnReaction.getAttribute('value');
+  const commentContainersToHighlight = commentContainersList
+    .filter(commentContainerElement => {
+      const lastComment = commentContainerElement.lastElementChild;
+      const hoorayEmotionElement = lastComment.querySelector('.js-reactions-container g-emoji[alias="tada"]');
 
-      if (checkReaction.indexOf(HOORAY_REACTION) >= 0 && checkMentor.indexOf(MENTOR_NAME) >= 0) {
-        return false;
-      } else {
-        element.lastElementChild.querySelector('.review-comment:last-child').style.backgroundColor = 'rgba(0, 255, 0, 0.3)';
-      }
+      if (!hoorayEmotionElement) {
+        return true;
+      };
+
+      const hoorayBtnElement = hoorayEmotionElement.parentElement;
+      const btnElementLabel = hoorayBtnElement.getAttribute('aria-label');
+
+      return btnElementLabel.indexOf(MENTOR_NAME) === -1;
     });
+
+  commentContainersToHighlight
+    .forEach(element => element.style.backgroundColor = 'rgba(0, 255, 0, 0.3)');
 })();
--- bookmarklet.js
+++ bookmarklet.js
@@ -9,53 +9,28 @@ javascript:
   const commentContainers = document.querySelectorAll('.js-comment-container');
   const commentContainersList = Array.prototype.slice.call(commentContainers);
 
-  const closedCommentContainersList = commentContainersList
+  const checkMentorHooray = (commentContainerElement) => {
+    const lastComment = commentContainerElement.lastElementChild;
+    const hoorayEmotionElement = lastComment.querySelector('.js-reactions-container g-emoji[alias="tada"]');
+
+    if (!hoorayEmotionElement) {
+      return true;
+    };
+
+    const hoorayBtnElement = hoorayEmotionElement.parentElement;
+    const btnElementLabel = hoorayBtnElement.getAttribute('aria-label');
+
+    return btnElementLabel.indexOf(MENTOR_NAME) === -1;
+  };
+  
+
+  commentContainersList
     .filter(element => element.classList.contains('outdated-comment'))
     .forEach(element => element.classList.remove('outdated-comment'));
 
-  const reactedCommentsList = commentContainersList 
-    .filter(element => element.querySelector('.js-reactions-container button.reaction-summary-item'))
-    // .filter(element => element.querySelector('.js-reactions-container button.reaction-summary-item').hasAttribute('value', 'HOORAY'))
-    // .filter(function(element) {
-    //   element.querySelector('.js-reactions-container button.reaction-summary-item').hasAttribute('value', 'HOORAY')
-    // })
-    .forEach(function(element) {
-      let reactionBtns = element.querySelector
-    })
+  const commentContainersToHighlight = commentContainersList
+    .filter(checkMentorHooray);
 
-  console.log(reactedCommentsList)
-
-  // console.log('Массив после фильтрации вернул мне: ' + reactedCommentsList)
-    // .forEach(function(element) {
-    //   const btnReactions = element.querySelectorAll('.js-reactions-container button.reaction-summary-item');
-    //   const btnReactionsList = Array.prototype.slice.call(btnReactions);
-
-    //   const hoorayReactionBtn = btnReactionsList
-    //     // .filter(element => element.hasAttribute('value', 'HOORAY'))
-    //     .filter(function(elemnt) {
-    //       console.log(element)
-    //       let checkMentor = element.getAttribute('aria-label');
-    //       let checkReaction = element.getAttribute('value');
-
-    //       if (checkReaction.indexOf(HOORAY_REACTION) >= 0 && checkMentor.indexOf(MENTOR_NAME) >= 0) {
-    //         return true;
-    //       } 
-    //     })
-    //     .forEach(function(element) {
-    //       let checkMentor = element.getAttribute('aria-label');
-    //       let checkReaction = element.getAttribute('value');
-
-    //       console.log(checkMentor);
-    //       console.log(checkReaction)
-    //     });
-
-      // let checkMentor = btnReaction.getAttribute('aria-label');
-      // let checkReaction = btnReaction.getAttribute('value');
-
-      // if (checkReaction.indexOf(HOORAY_REACTION) >= 0 && checkMentor.indexOf(MENTOR_NAME) >= 0) {
-      //   return false;
-      // } else {
-      //   element.lastElementChild.querySelector('.review-comment:last-child').style.backgroundColor = 'rgba(0, 255, 0, 0.3)';
-      // }
-    });
+  commentContainersToHighlight
+    .forEach(element => element.style.backgroundColor = 'rgba(0, 255, 0, 0.3)');
 })();
